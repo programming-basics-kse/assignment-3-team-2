a800fb23b5e920fb24a3abd87f690c8c9bf1207e
yakiv <baiduck2015@gmail.com>
2024-11-18 10:52:51 +0200

    Sketch medals action with core methods

--- actions.py
+++ actions.py
@@ -1,2 +1,14 @@
-def medals(options):
+def fetch_lines_from_file(file):
     pass
+
+def filter_lines(lines, country, year):
+    pass
+
+def format_output():
+    pass
+
+def medals(options):
+    lines = fetch_lines_from_file(options.file)
+    filtered = filter_lines(lines, options.country, options.year)
+    
+    return format_output(filtered)
