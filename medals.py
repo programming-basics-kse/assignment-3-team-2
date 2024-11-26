d91a945dc49a5cb927e49ba96891414ce81b17a6
MikeIsupov <misupov@kse.org.ua>
2024-11-26 21:13:41 +0200

    finished display function

--- medals.py
+++ medals.py
@@ -1,9 +1,12 @@
+from sysconfig import get_path_names
+
+
 class Medal:
     def __init__(self, params):
         self.params = params
 
     def display(self) -> str:
-        pass
+        return f'{self.get_name()} {self.get_event()} {self.get_medal_type()}'
 
     def get_country(self) -> str:
         return self.params['team']
@@ -13,3 +16,9 @@ class Medal:
 
     def get_medal_type(self) -> str:
         return self.params['medal']
+
+    def get_name(self) -> str:
+        return self.params['name']
+
+    def get_event(self):
+        return self.params['event']
