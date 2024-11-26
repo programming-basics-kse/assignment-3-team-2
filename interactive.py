3a8791a7aa590c96a6100fcb46e9e96750dfb294
yakiv <baiduck2015@gmail.com>
2024-11-27 00:11:41 +0200

    Sketch interactive.py

---
+++ interactive.py
@@ -0,0 +1,19 @@
+class CountryStats:
+    def __init__(self, country):
+        self.country = country
+        #data here
+
+    def add_medal(self):
+        pass
+
+    def get_best_games(self) -> str:
+        pass
+
+    def get_worst_games(self) -> str:
+        pass
+
+    def get_average_medals(self, medal_type) -> float:
+        pass
+
+def process(options):
+    pass
