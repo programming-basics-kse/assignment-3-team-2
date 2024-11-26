44426dfef36d13b4bed65bc66f30640de1135c9e
MikeIsupov <misupov@kse.org.ua>
2024-11-27 01:47:36 +0200

    finished CountryStats

--- interactive.py
+++ interactive.py
@@ -1,8 +1,12 @@
+from actions import overall
+from medal import file_line_to_medal
+from medal import Medal
+
 class CountryStats:
     def __init__(self, country):
         self.country = country
-        self.most_medals = 0
-        self.least_medals = 0
+        self.most_medals = {'year': None, 'total_medals': 0}
+        self.least_medals = {'year': None, 'total_medals': 0}
         self.data = {
             'year': {
                 'place': None,
@@ -14,19 +18,35 @@ class CountryStats:
     def add_medal(self, medal):
         if medal.get_medal_type() == 'NA' or self.country != options.countries:
             return
-        self.data[]
+        year = medal.get_year()
+        total_medals = self.data[year]['total_medals']
+
+        self.data[year]['medals'][medal.get_medal_type] += 1
+        total_medals += 1
+
+        if total_medals > self.most_medals['total_medals']:
+            self.most_medals['total_medals'] = total_medals
+            self.most_medals['year'] = year
+        if total_medals < self.least_medals['total_medals']:
+            self.least_medals['total_medals'] = total_medals
+            self.least_medals['year'] = year
 
     def get_best_games(self) -> str:
-        return f'{self.data[]}'
+        return f"Best games: {self.most_medals['year']} - {self.most_medals['total_medals']} medals\n"
 
     def get_worst_games(self) -> str:
-        pass
+        return f"Worst games: {self.least_medals['year']} - {self.least_medals['total_medals']} medals\n
 
     def get_average_medals(self) -> str:
-        pass
+        overall_years = len(self.data)
+        overall_medals = sum(self.data[year]['total_medals'] for year in self.data)
+        average_medals = round(overall_medals / overall_years, 2)
+        return f'Average medals: {average_medals}\n'
+
 
     def get_first_participation(self) -> str:
-        pass
+        first_year = min(self.data.keys())
+        return f"First participation: {first_year} {self.data['year']['place']}\n"
 
 def format_output(stats):
     output = f'The first olympics participation: {stats.get_first_participation()\n}'
@@ -59,4 +79,3 @@ def process(options):
         result = process_country(inp, options)
         print(result, end='')
         output += result
-    
