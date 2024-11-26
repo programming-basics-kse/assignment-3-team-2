bbec5fec47e46194c2526fb103c3b3e64026ff07
MikeIsupov <misupov@kse.org.ua>
2024-11-26 22:11:32 +0200

    added CountryStats

--- total.py
+++ total.py
@@ -1,9 +1,18 @@
+from medal import Medal
+
 class CountryStats:
     def __init__(self, country: str):
-        pass #setup medal stats
+        self.country = country
+        self.medals = {
+            'gold': 0,
+            'silver': 0,
+            'bronze': 0
+        }
 
-    def add_medal(self, medal: Medal):
-        pass #takes a medal, updates stats
+    def add_medal(self, medal: Medal):#takes a medal, updates stats
+        if medal.get_medal_type() == 'NA':
+            return
+        self.medals[medal.get_medal_type()] += 1
 
 def process(options) -> str :
     pass
