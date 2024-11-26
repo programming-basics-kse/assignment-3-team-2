3ce62616371ce216d471fc1d8b4e5aaf21135b2b
yakiv <baiduck2015@gmail.com>
2024-11-26 23:07:56 +0200

    Sketch overall.py

--- actions.py
+++ actions.py
@@ -1,8 +1,12 @@
 import medals as medals_action
 import total as total_action
+import overall as overall_action
 
 def medals(options):
     return medals_action.process(options)
 
 def total(options):
     return total_action.process(options)
+
+def overall(options):
+    return overall_action.process(options)
---
+++ overall.py
@@ -0,0 +1,7 @@
+def format_output(result) -> str:
+    pass
+
+def process(options):
+    result = {} #country: [best_year, n_medals]
+    #do some stuff
+    return format_output(result)
