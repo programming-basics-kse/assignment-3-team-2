fd6b3a092930aa2867a711203961c59bb0ebb09b
MikeIsupov <misupov@kse.org.ua>
2024-11-27 00:41:27 +0200

    working on CountryStats

--- interactive.py
+++ interactive.py
@@ -1,23 +1,28 @@
 class CountryStats:
     def __init__(self, country):
         self.country = country
-        # data = {
-        #    year: {
-        #       place: value,
-        #       medals: {gold: n, silver: n, bronze: n}
-        #   }
-        #}
+        self.most_medals = 0
+        self.least_medals = 0
+        self.data = {
+            'year': {
+                'place': None,
+                'total_medals': 0,
+                'medals': {'gold': 0, 'silver': 0, 'bronze': 0}
+            }
+        }
 
-    def add_medal(self):
-        pass
+    def add_medal(self, medal):
+        if medal.get_medal_type() == 'NA' or self.country != options.countries:
+            return
+        self.data[]
 
     def get_best_games(self) -> str:
-        pass
+        return f'{self.data[]}'
 
     def get_worst_games(self) -> str:
         pass
 
-    def get_average_medals(self, medal_type) -> float:
+    def get_average_medals(self) -> str:
         pass
 
     def get_first_participation(self) -> str:
