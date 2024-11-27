acad59f76a0ef33c909464e8ab6c1a9ee94d4f40
yakiv <baiduck2015@gmail.com>
2024-11-27 12:09:43 +0200

    Fix everything that's broken. Finish interactive mode

--- interactive.py
+++ interactive.py
@@ -1,4 +1,3 @@
-from actions import overall
 from medal import file_line_to_medal
 from medal import Medal
 
@@ -7,20 +6,25 @@ class CountryStats:
         self.country = country
         self.most_medals = {'year': None, 'total_medals': 0}
         self.least_medals = {'year': None, 'total_medals': 0}
-        self.data = {
-        #    'year': {
-        #        'city': None,
-        #        'total_medals': 0,
-        #        'medals': {'gold': 0, 'silver': 0, 'bronze': 0}
-        #    }
+        self.data = {}
+        self.data_template = { #each year has this
+            'city': None,
+            'total_medals': 0,
+            'medals': {'gold': 0, 'silver': 0, 'bronze': 0}
         }
+
+    def is_valid(self) -> bool:
+        return len(self.data) > 0
 
     def add_medal(self, medal):
         if medal.get_medal_type() == 'NA' or self.country not in [medal.get_country(), medal.get_noc()]:
             return
 
         year = medal.get_year()
-        total_medals = 
+        if year not in self.data:
+            self.data[year] = self.data_template
+            self.data[year]['city'] = medal.get_city()
+
         self.data[year]['medals'][medal.get_medal_type().lower()] += 1
         self.data[year]['total_medals'] += 1
         total_medals = self.data[year]['total_medals']
@@ -28,7 +32,7 @@ class CountryStats:
         if total_medals > self.most_medals['total_medals']:
             self.most_medals['total_medals'] = total_medals
             self.most_medals['year'] = year
-        if total_medals < self.least_medals['total_medals']:
+        if total_medals < self.least_medals['total_medals'] or self.least_medals['year'] == None:
             self.least_medals['total_medals'] = total_medals
             self.least_medals['year'] = year
 
@@ -51,12 +55,12 @@ class CountryStats:
 
     def get_first_participation(self) -> str:
         first_year = min(self.data.keys())
-        return f"First participation: {first_year} {self.data['year']['place']}\n"
+        return f"First participation: {first_year} {self.data[first_year]['city']}\n"
 
 def format_output(stats):
     output = stats.get_first_participation()
-    output += stats.get_best_games()}
-    output += stats.get_worst_games()}
+    output += stats.get_best_games()
+    output += stats.get_worst_games()
     output += stats.get_average_medals()
     
     return output
@@ -71,8 +75,11 @@ def process_country(country, options):
         line = file.readline().replace('\n', '')
         while line:
             medal = file_line_to_medal(header, line)
-            stats.add_medal()
+            stats.add_medal(medal)
             line = file.readline().replace('\n', '')
+    
+    if not stats.is_valid():
+        return 'Sorry, the country you provided was not found in the dataset\n'
 
     return format_output(stats)
 
@@ -80,10 +87,13 @@ def process(options):
     output = ''
     print('Type q to exit')
     inp = input('(Country) ')
-    while inp != q:
+    while inp != 'q':
         result = process_country(inp, options)
+        result += '-' * 10 + '\n'
         print(result, end='')
         output += inp
         output += result
+        
+        inp = input('(Country) ')
 
     return output
